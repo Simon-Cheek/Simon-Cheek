056c87e5c2d0de23dad91731cd6baf0639015a5a
Simon Cheek <simon.cheek@yahoo.com>
2024-01-18 10:35:24 -0700

    add error handling cat API

--- cat.js
+++ cat.js
@@ -11,40 +11,46 @@ let isHeader = false;
 
 const addFact = async () => {
 
-    // Turn off event listener
-    console.log('reached!');
-    deactivate();
+    try {
 
-    // Add a header if not existant
-    if (isHeader == false) {
-        // catHeader.innerHTML = 'Cat Facts';
-        let catHeader = document.createElement('h3');
-        let lineBreak = document.createElement('hr');
-        let spacing = document.createElement('br');
+        // Turn off event listener
+        console.log('reached!');
+        deactivate();
 
-        catHeader.classList.add('intro-header', 'cat-header');
-        catHeader.innerText = 'Cat Facts';
+        // Add a header if not existant
+        if (isHeader == false) {
+            // catHeader.innerHTML = 'Cat Facts';
+            let catHeader = document.createElement('h3');
+            let lineBreak = document.createElement('hr');
+            let spacing = document.createElement('br');
 
-        catHeaderContainer.append(spacing, spacing);
-        catHeaderContainer.append(lineBreak);
-        catHeaderContainer.append(spacing);
-        catHeaderContainer.append(catHeader);
+            catHeader.classList.add('intro-header', 'cat-header');
+            catHeader.innerText = 'Cat Facts';
 
-        isHeader = true;
+            catHeaderContainer.append(spacing, spacing);
+            catHeaderContainer.append(lineBreak);
+            catHeaderContainer.append(spacing);
+            catHeaderContainer.append(catHeader);
+
+            isHeader = true;
+        }
+
+        // Get Cat Fact and store in variable
+        const response = await fetch("https://catfact.ninja/fact");
+        const fact = await response.json();
+
+        // Add to List
+        let newLi = document.createElement('li');
+        newLi.classList.add('gen-h',);
+        newLi.innerText = fact.fact;
+        catList.append(newLi);
+
+        // Reactivate Event Listener
+        activate();
+
+    } catch {
+        alert('Unable to connect to CatFact API');
     }
-
-    // Get Cat Fact and store in variable
-    const response = await fetch("https://catfact.ninja/fact");
-    const fact = await response.json();
-
-    // Add to List
-    let newLi = document.createElement('li');
-    newLi.classList.add('gen-h',);
-    newLi.innerText = fact.fact;
-    catList.append(newLi);
-
-    // Reactivate Event Listener
-    activate();
 }
 
 
